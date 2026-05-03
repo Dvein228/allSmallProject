7a519977d153161c6a69322d70bd24b1505d97f0
Home <davidfilippov122011@gmail.com>
2026-05-03 12:33:53 +0300

    оп начало

---
+++ Задание 2.py
@@ -0,0 +1,6 @@
+def NechetnieChisla(list):
+    for i in list:
+        if i%2!=0:
+            print(i)
+list2=[1,2,3,4,5]
+NechetnieChisla(list2)
---
+++ Задание 3.py
@@ -0,0 +1,8 @@
+def Sumaaa(list):
+    sumn_num=0
+    for i in list:
+        sumn_num+=i
+    print(sumn_num)
+
+list=[1,2,3]
+Sumaaa(list)
