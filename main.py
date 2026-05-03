9ec5738009aa092898bd529333b2d5bc15d45b5a
Home <davidfilippov122011@gmail.com>
2026-05-03 14:09:52 +0300

    оп начало

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-list23 = [1,2,3,4,5,6]
-sums_nums=0
-for i in list23:
-     sums_nums+=i
-print(sums_nums)
+class Dog:
+     def __init__(self,name,age,health):
+          self.name= name
+          self.age = age
+          self.health=health
+     def inroduce(self):
+          print(f"Собака:{self.name},Возраст {self.age}.Собака {self.health}")
+
+
+
+soBAKAAAAA = Dog("Бобиков","15","здорова")
+soBAKAAAAA2= Dog("Крутая","13","не здорова")
+soBAKAAAAA.inroduce()
+soBAKAAAAA2.inroduce()
