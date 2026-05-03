faffa73c52e4f53f9958d426d8638ee77342f275
Home <davidfilippov122011@gmail.com>
2026-05-03 14:31:12 +0300

    задание на ооп

---
+++ CLASS5.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, name, level):
+        self.name=name
+        self.level=level
+
+    def info(self):
+        print("Ім'я:", self.name)
+        print("Рівень:", self.level)
+
+LDFGFDGFDGD=Character("Гном-перегном","5")
+drugoi = Character("Гном","7")
+EsheDrugoi = Character("НеГном","1")
