a602db971872dde3e7761fc07d6210fa309ee031
Home <davidfilippov122011@gmail.com>
2026-05-10 12:58:17 +0300

    пара5

--- CLASS5.py
+++ CLASS5.py
@@ -1,11 +1,4 @@
-class Character:
-    def __init__(self, name, level):
-        self.name=name
-        self.level=level
 
-    def info(self):
-        print("Ім'я:", self.name)
-        print("Рівень:", self.level)
 class Weapon:
     def __init__(self, name, damage):
         self.name=name
@@ -15,10 +8,19 @@ class Weapon:
         print("Ім'я:", self.name)
         print("Урон:", self.damage)
 
-LDFGFDGFDGD=Character("Гном-перегном","5")
-drugoi = Character("Гном","7")
-EsheDrugoi = Character("НеГном","1")
+class Character:
+    def __init__(self, name, level,weapon):
+        self.name=name
+        self.level=level
+        self.weapon = weapon
+
+    def info(self):
+        print("Ім'я:", self.name)
+        print("Рівень:", self.level)
 
 Bow= Weapon("лук","1")
 Axe= Weapon("Акс","375")
 
+LDFGFDGFDGD=Character("Гном-перегном","5",Bow)
+drugoi = Character("Гном","7",Bow)
+EsheDrugoi = Character("НеГном","1",Axe)
