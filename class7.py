d7f8af2babf259f85dd2c482b1edb443aa6dd644
Home <davidfilippov122011@gmail.com>
2026-05-17 12:51:28 +0300

    пара6

--- class7.py
+++ class7.py
@@ -6,23 +6,21 @@ class Animal:
         print("Ім'я:", self.name)
         print("Вік:", self.age)
     def voice(self,sound):
-        print(self.name+sound)
+        print(f"{self.name} делает {sound}")
 
 class Cat(Animal):
     def __init__(self,wool,age,name):
         super().__init__(age, name)
         self.wool = wool
-    def meow(self):
-        print(self.name+":мяу")
+
 class Fish(Animal):
     def __init__(self,age,name,scales):
         super().__init__(age,name)
         self.scales= scales
-    def bulk(self):
-        print(self.name+":буль")
+
 
 
 carpik = Fish(1,"каРПик",True)
 cotak = Cat(True,7,"Котак")
-carpik.bulk()
-cotak.meow()
+carpik.voice("Бульк")
+cotak.voice("Мяу")
