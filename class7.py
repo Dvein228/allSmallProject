64827a4ceb348ebfb4a3226b765a6300e0a50ab5
Home <davidfilippov122011@gmail.com>
2026-05-17 12:23:59 +0300

    пара6

---
+++ class7.py
@@ -0,0 +1,12 @@
+class animal:
+    def __init__(self,age,name):
+        self.age=age
+        self.name=name
+    def info(self):
+        print("Ім'я:", self.name)
+        print("Вік:", self.age)
+
+class cat(animal):
+    def __init__(self,wool,age,name):
+        super().__init__(age, name)
+        self.wool = wool
