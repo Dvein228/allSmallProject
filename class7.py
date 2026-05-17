442c1b14fd2349936c9333cc86c070b750c19335
Home <davidfilippov122011@gmail.com>
2026-05-17 12:47:31 +0300

    пара6

--- class7.py
+++ class7.py
@@ -10,10 +10,14 @@ class Cat(Animal):
     def __init__(self,wool,age,name):
         super().__init__(age, name)
         self.wool = wool
+    def meow(self):
+        print(self.name+":мяу")
 class Fish(Animal):
     def __init__(self,age,name,scales):
         super().__init__(age,name)
         self.scales= scales
+    def bulk(self):
+        print(self.name+":буль")
 
 
 carpik = Fish(1,"каРПик","присутствует")
