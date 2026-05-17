8377d0fac5ee78ca4d91305449ca8584fab8d73d
Home <davidfilippov122011@gmail.com>
2026-05-17 12:02:25 +0300

    пара6

--- Задание 1.py
+++
@@ -1,6 +0,0 @@
-n=int(input("первое число"))
-n2 = int(input("второе число"))
-if n>n2:
-    print(n)
-elif n<n2:
-    print(n2)
--- Задание 3.py
+++
@@ -1,8 +0,0 @@
-def Sumaaa(list):
-    sumn_num=0
-    for i in list:
-        sumn_num+=i
-    print(sumn_num)
-
-list=[1,2,3]
-Sumaaa(list)
